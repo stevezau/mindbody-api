8d7404d14e4dc23b0f11f3e4dcb8dd7b84b5806f
Steve Adams <stevezau@gmail.com>
2018-08-25 17:34:02 +1000

    sale fixes

--- src/sales.js
+++ src/sales.js
@@ -3,18 +3,43 @@ import moment from 'moment-timezone';
 import MindbodyBase from './base';
 
 function parseTable($, table, sales) {
-  let rep = table.children('caption').text().trim().split(',', 2);
-  rep = (rep.length === 2 ? `${rep[1]} ${rep[0]}` : `${rep[0]}`).trim();
+  let payMethod = table.children('caption').text().trim();
+
+  // Reformat PayMethod
+  const payFormat = payMethod.match(/\((.+?)-/);
+  if (payFormat) {
+    payMethod = payFormat[1]; // eslint-disable-line
+  }
+
+
+  // Figure out the col mappings
+  const headers = Array.from(table.first('thead').find('th'));
+  const findHeader = name => (
+    headers.findIndex(h => $(h).text().trim().toLowerCase() === name.toLowerCase())
+  );
+
+  const index = {
+    saleId: findHeader('Sale ID'),
+    client: findHeader('Client'),
+    item: findHeader('Item Name'),
+    subtotal: findHeader('Subtotal (excluding tax)'),
+    discountAmount: findHeader('Discount amount'),
+    discountPercent: findHeader('Discount %'),
+    tax: findHeader('Tax'),
+    total: findHeader('Item Total'),
+    paid: findHeader('Total Paid w/ Payment Method'),
+    quantity: findHeader('Quantity'),
+  };
 
   table.children('tbody').children('tr').each((i, tr) => {
     let tds = $(tr).children('td').map((_, td) => $(td));
-    const saleId = Number($(tds[0]).text());
+    const saleId = Number($(tds[index.saleId]).text());
     if (Number.isNaN(saleId)) {
       return; // Probably html table thead
     }
 
     // Handle case where item name has a < > which causes issues with parsing via cheerio
-    if (!$(tds[5]).text()) {
+    if (!$(tds[index.item]).text()) {
       const html = $(tr).html();
       const found = html.match(/(itemnameCell">)(.+)(<="" td="">)/);
       if (!found) return;
@@ -24,15 +49,13 @@ function parseTable($, table, sales) {
     }
 
     if (!sales[saleId]) {
-      sales[saleId] = { Units: [] }; // eslint-disable-line
+      sales[saleId] = { ID: saleId, Units: [] }; // eslint-disable-line
     }
     const sale = sales[saleId];
 
-    sale.Rep = rep;
-
     if (!sale.client) {
-      const clientID = $(tds[2]).find('a').attr('href').match(/ID=([0-9]+)/);
-      const clientArr = $(tds[2]).text().trim().split(',', 2);
+      const clientID = $(tds[index.client]).find('a').attr('href').match(/ID=([0-9]+)/);
+      const clientArr = $(tds[index.client]).text().trim().split(',', 2);
       sale.Client = {
         ID: Number(clientID[1]),
         FirstName: (clientArr.length === 2 ? clientArr[1] : clientArr[0]).trim(),
@@ -43,14 +66,15 @@ function parseTable($, table, sales) {
     const dollarRegex = /[%$,]/g;
 
     sale.Units.push({
-      Name: $(tds[3]).text().trim(),
-      Price: parseFloat($(tds[5]).text().replace(dollarRegex, '')),
-      Quantity: Number($(tds[6]).text().trim()),
-      Subtotal: parseFloat($(tds[7]).text().replace(dollarRegex, '')),
-      DiscountPercent: parseFloat($(tds[8]).text().replace(dollarRegex, '')),
-      DiscountAmount: parseFloat($(tds[9]).text().replace(dollarRegex, '')),
-      Tax: parseFloat($(tds[10]).text().replace(dollarRegex, '')),
-      Total: parseFloat($(tds[11]).text().replace(dollarRegex, ''))
+      Name: $(tds[index.item]).text().trim(),
+      Quantity: Number($(tds[index.quantity]).text().trim()),
+      Subtotal: parseFloat($(tds[index.subtotal]).text().replace(dollarRegex, '')),
+      DiscountPercent: parseFloat($(tds[index.discountPercent]).text().replace(dollarRegex, '')),
+      DiscountAmount: parseFloat($(tds[index.discountAmount]).text().replace(dollarRegex, '')),
+      Tax: parseFloat($(tds[index.tax]).text().replace(dollarRegex, '')),
+      Total: parseFloat($(tds[index.total]).text().replace(dollarRegex, '')),
+      Paid: parseFloat($(tds[index.paid]).text().replace(dollarRegex, '')),
+      Payment: payMethod,
     });
   });
 }
@@ -67,6 +91,60 @@ export default class Sales extends MindbodyBase {
         dateTime: text => moment.tz(text, this.timezone),
         date: text => moment.tz(text, this.timezone)
       }
+    });
+  }
+
+  getSalesWeb(fromDate, toDate) {
+    const form = {
+      hPostAction: 'Generate',
+      'sr-range-opt': '',
+      'sr-name': '',
+      autogenerate: 'hasGenerated',
+      reportUrl: '/Report/Sales/Sales',
+      category: 'Sales',
+      requiredtxtDateStart: moment.tz(fromDate, this.timezone).format('YYYY/MM/DD'),
+      requiredtxtDateEnd: moment.tz(toDate, this.timezone).format('YYYY/MM/DD'),
+      optFilterTagged: 'false',
+      optSaleLoc: [],
+      optHomeStudio: [],
+      optPayMethod: [],
+      optCategory: [],
+      optEmployee: '',
+      optRep: '0',
+      optIncludeAutoRenews: 'Include',
+      optDisMode: 'Detail',
+      optBasis: 'AccrualBasis',
+      optIncludeReturns: 'true',
+      optShowSupplier: 'false',
+      quickDateSelectionTwo: ''
+    };
+
+    return new Promise((resolve, reject) => {
+      console.log('Running SalesWeb from:', form.requiredtxtDateStart, 'to:', form.requiredtxtDateEnd);
+      this.get('https://clients.mindbodyonline.com/Report/Sales/Sales')
+        .then((rsp) => {
+          const $ = cheerio.load(rsp.body);
+          $('#optSaleLoc option').each((i, el) => form.optSaleLoc.push($(el).attr('value')));
+          $('#optHomeStudio option').each((i, el) => form.optHomeStudio.push($(el).attr('value')));
+          $('#optPayMethod option').each((i, el) => form.optPayMethod.push($(el).attr('value')));
+          return this.get('https://clients.mindbodyonline.com/Ajax/GetCategoriesByType');
+        })
+        .then((rsp) => {
+          const $ = cheerio.load(rsp.body);
+          $('option').each((i, el) => {
+            form.optCategory.push($(el).attr('value'));
+          });
+          return this.post('https://clients.mindbodyonline.com/Report/Sales/Sales/Generate?reportID=undefined', form);
+        })
+        .then((rsp) => {
+          const sales = {};
+          const $ = cheerio.load(rsp.body);
+          $('table.result-table').each((i, el) => {
+            parseTable($, $(el), sales);
+          });
+          resolve(sales);
+        })
+        .catch(err => reject(err));
     });
   }
 
@@ -112,8 +190,20 @@ export default class Sales extends MindbodyBase {
         .then((rsp) => {
           const sales = {};
           const $ = cheerio.load(rsp.body);
-          $('table.result-table').each((i, el) => {
-            parseTable($, $(el), sales);
+
+          $('table.result-table').each((i, elem) => {
+            const table = $(elem);
+            const headers = Array.from(table.first('thead').find('th'));
+            const saleIndex = headers.findIndex(h => $(h).text().trim().toLowerCase() === 'sale id');
+            table.children('tbody').children('tr').each((_, tr) => {
+              const tds = $(tr).children('td').map((idx, td) => $(td));
+              const saleId = Number($(tds[saleIndex]).text());
+              if (!Number.isNaN(saleId)) {
+                let rep = table.children('caption').text().trim().split(',', 2);
+                rep = (rep.length === 2 ? `${rep[1]} ${rep[0]}` : `${rep[0]}`).trim();
+                sales[saleId] = rep;
+              }
+            });
           });
           resolve(sales);
         })
@@ -131,23 +221,23 @@ export default class Sales extends MindbodyBase {
       console.log('Getting sales via soap from:', fromDate.toISOString(), 'to:', toDate.toISOString());
       this.soapReq('GetSales', 'GetSalesResult', req)
         .then((result) => {
+          const sales = {};
+
           if (result.Sales) {
-            let sales = result.Sales.Sale.filter(s => s.Payments !== null);
-            sales = sales.map((sale) => {
-              /* eslint-disable */
-              sale.ID = Number(sale.ID);
-              sale.Payments = sale.Payments.Payment.map((payment) => {
-                payment.ID = Number(payment.ID);
-                payment.Amount = parseFloat(payment.Amount);
+            result.Sales.Sale.filter(s => s.Payments !== null).forEach((s) => {
+              const sale = { ...s };
+              sale.ID = Number(s.ID);
+              sale.ClientID = Number(s.ClientID);
+              sale.Payments = s.Payments.Payment.map((p) => {
+                const payment = { ...p };
+                payment.ID = Number(p.ID);
+                payment.Amount = parseFloat(p.Amount);
                 return payment;
               });
-              /* eslint-enable */
-              return sale;
+              sales[s.ID] = sale;
             });
-            resolve({ sales, countTotal: result.ResultCount, pagesTotal: result.TotalPageCount });
-          } else {
-            resolve([]);
           }
+          resolve({ sales, countTotal: result.ResultCount, pagesTotal: result.TotalPageCount });
         })
         .catch(err => reject(err));
     });
@@ -205,18 +295,29 @@ export default class Sales extends MindbodyBase {
 
   getSales(fromDate, toDate) {
     return new Promise((resolve, reject) => {
-      Promise.all([this.getSalesSoap(fromDate, toDate), this.getRepSales(fromDate, toDate)])
+      const getData = [
+        this.getSalesWeb(fromDate, toDate),
+        this.getSalesSoap(fromDate, toDate),
+        this.getRepSales(fromDate, toDate)
+      ];
+
+      Promise.all(getData)
         .then((result) => {
+          const webSales = result[0];
+          const soapSales = result[1].sales || {};
+          const repSales = result[2];
           const sales = [];
-          if (result[0].sales) {
-            result[0].sales.forEach((sale) => {
-              if (!result[1][Number(sale.ID)]) {
-                console.log(`Sale ${sale.ID} was not found`, fromDate, toDate);
-              } else {
-                sales.push(Object.assign(result[1][Number(sale.ID)], sale));
-              }
-            });
-          }
+
+          Object.values(webSales).forEach((s) => {
+            const sale = { ...s };
+            if (!soapSales[sale.ID]) {
+              console.log(`Sale ${sale.ID} was not found in SOAP`, fromDate, toDate);
+            } else {
+              sale.Rep = repSales[sale.ID] || 'Unknown Staff';
+              sales.push(Object.assign(soapSales[Number(sale.ID)], sale));
+            }
+          });
+
           resolve({
             sales,
             countTotal: result[0].ResultCount,
